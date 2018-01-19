e65cef73f94ec3c22f8d83e3b708a14902333d0c
Celeste Gerard <celeste.gerard@gmail.com>
2018-01-19 21:42:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var name = "Susan"
 var height = 70
 
 if (name === "Jonah"){
-  console.log("The name is correct")
+  console.log("The name is correct");
 } else {
-  console.log "Expected to be Jonah."
+  console.log("Expected to be Jonah.");
 }
 
 // Don't worry about this
