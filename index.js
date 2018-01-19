a8a4807160ce89b9534513c01ca02070e0bffeac
Celeste Gerard <celeste.gerard@gmail.com>
2018-01-19 21:42:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var height = 70
 
 if (name === "Jonah"){
   console.log("The name is correct")
-} else{
-  return "Expected to be Jonah."
+} else {
+  console.log "Expected to be Jonah."
 }
 
 // Don't worry about this
